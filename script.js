2666c1b4373d6e354af57c799fda2cc877d5a5bb
Raj Ramesh Bengade <157134976+rajbengade07@users.noreply.github.com>
2025-04-28 14:04:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ function greet()
 			let d=new Date();
 			let hr=d.getHours();
 			let result="";
-			if(hr<12)
-				result= "Good Morning " + name.value;
-			else if(hr<16)
-				result="Good Afternoon " + name.value;
-			else
-				result="Good Evening " + name.value;
+			if (hr < 12)
+    				result = "🌞 Good Morning " + name.value;
+ 			else if (hr < 16)
+   				result = "☀️ Good Afternoon " + name.value;
+  			else
+    				result = "🌙 Good Evening " + name.value;
 			msg.innerHTML=result;
 		}
